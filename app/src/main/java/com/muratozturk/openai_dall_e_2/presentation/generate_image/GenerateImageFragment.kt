dd5c946a26a0b6284e795760d8bce65508834b1f
Sayman Rifat <saymanrifat@gmail.com>
2023-11-20 17:01:15 +0600

    All OK

--- app/src/main/java/com/muratozturk/openai_dall_e_2/presentation/generate_image/GenerateImageFragment.kt
+++ app/src/main/java/com/muratozturk/openai_dall_e_2/presentation/generate_image/GenerateImageFragment.kt
@@ -4,6 +4,7 @@ package com.muratozturk.openai_dall_e_2.presentation.generate_image
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -28,8 +29,6 @@ class GenerateImageFragment : Fragment(R.layout.fragment_generate_image) {
 
     private val viewModel: GenerateImageViewModel by viewModels()
     private val binding by viewBinding(FragmentGenerateImageBinding::bind)
-
-
     private val viewModelWeather by viewModels<MainViewModel>()
     private var edTextStr: String = ""
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -54,16 +53,17 @@ class GenerateImageFragment : Fragment(R.layout.fragment_generate_image) {
                     binding.temp.text = tempStr
 
                     val conditionStr = "Condition : " + it.data?.current?.condition?.text
-                    binding.conditions.text
-                    val promptStr: String =
+                    binding.conditions.text = conditionStr
+                    val promptStr =
                         "Create Image in $locationStr on weather of $conditionStr"
                     sendToAi(promptStr)
                 }
 
                 is ResultData.Failed -> {
                     Log.d("TAG", "onCreate: failed ${it.message}")
+                    Toast.makeText(context, "No Weather Data", Toast.LENGTH_SHORT)
+                        .show()
                 }
-
                 is ResultData.Loading -> {
                     Log.d("TAG", "onCreate: Loading")
                 }
@@ -72,25 +72,37 @@ class GenerateImageFragment : Fragment(R.layout.fragment_generate_image) {
     }
 
     private fun sendToAi(promptTextFromWeather: String) {
-
-
         with(viewModel) {
             with(binding) {
+                weatherLoading.gone()
+                aiLoading.visible()
                 if (promptTextFromWeather.isEmpty().not()) {
                     generateImage(promptEditText.text.toString(), 1, Sizes.SIZE_256)
+                    aiLoading.gone()
                 } else {
                     promptInputLayout.error = getString(R.string.enter_prompt)
+                    Toast.makeText(context, getString(R.string.enter_prompt), Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
         }
-
     }
 
     private fun initViewCollect() {
         with(viewModel) {
             with(binding) {
                 generateButton.setOnClickListener {
-                    getTextFromEdittext()
+                    weatherLoading.visible()
+                    if (promptEditText.text.toString().isEmpty().not()) {
+                        getTextFromEdittext()
+                    } else {
+                        promptInputLayout.error = getString(R.string.enter_prompt)
+                        Toast.makeText(
+                            context,
+                            getString(R.string.enter_prompt),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
                 generatedImageCard.applyClickShrink()
 
