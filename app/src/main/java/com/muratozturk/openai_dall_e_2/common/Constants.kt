6a41b96a38a6ff03954b5a18acc513058a046c23
Sayman Rifat <saymanrifat@gmail.com>
2023-11-26 16:09:57 +0600

    All OK

--- app/src/main/java/com/muratozturk/openai_dall_e_2/common/Constants.kt
+++ app/src/main/java/com/muratozturk/openai_dall_e_2/common/Constants.kt
@@ -2,7 +2,7 @@ package com.muratozturk.openai_dall_e_2.common
 
 object Constants {
     const val BASE_URL = "https://api.openai.com/v1/"
-    const val TOKEN = "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"
+    const val TOKEN = ""
     const val GENERATE_IMAGE = "images/generations"
     const val SIZE_256 = "256x256"
     const val SIZE_512 = "512x512"
